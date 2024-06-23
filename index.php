bff0dc81d189e6462d38926e2474f1e27d2ed658
Yudha Ajie <yudhaajie@gmail.com>
2024-06-23 21:12:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * CodeIgniter
  *
  * An open source application development framework for PHP
- *
+ *s
  * This content is released under the MIT License (MIT)
  *
  * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
