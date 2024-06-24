4a9a2671ce0ef9855f092f4c2f85ef753dd08e45
Yudha Ajie <yudhaajie@gmail.com>
2024-06-24 10:35:04 +0700

    welcome

--- application/views/frontend/comingsoon.php
+++ application/views/frontend/comingsoon.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Coming Soon</title>
+	<title>Coming Soon Tester</title>
 	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/admin/vendor_assets/css/bootstrap/bootstrap.css">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/admin/vendor_assets/css/daterangepicker.css">
