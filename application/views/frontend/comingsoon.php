5da74372dbf9aa32dd696edb54f2dd4846bce848
Yudha Ajie <yudhaajie@gmail.com>
2024-06-24 15:16:11 +0700

    kash=ih paham

--- application/views/frontend/comingsoon.php
+++ application/views/frontend/comingsoon.php
@@ -42,7 +42,7 @@
 				<div class="bg-white content-center">
 					<div class="coming-soon text-center">
 						<img class="img-fluid" src="assets/images/logo.png?v=101"><br>
-						hai coba dongsa
+						hai coba dongsa test
 						</div>
 					</div>
 				</div>
